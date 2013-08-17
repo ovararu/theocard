488d6356ce9928a28983cb499c6e062e64931413
Octavian Vararu <ovararu@yahoo.com>
2013-08-17 13:06:19 +0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Theocard::Application.config.secret_key_base = '912061f8a0580c302a6a76dfcffedac61d4a5f5babd540cfd1ba9119a9a82ec6a032b1aa11eab88653a86c6371c6cb5c1ef088ce3a21c96cf0136551ed16ce72'
